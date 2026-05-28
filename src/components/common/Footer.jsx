7ab97f32b62da1577818dffcea82d6ef7c853b28
Vedang1994 <87564842+Vedang1994@users.noreply.github.com>
2026-05-28 17:32:29 +0100

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,11 +5,7 @@ export default function Footer() {
       <div className="container">
 
 <div className="footer-logo">
-  <img
-    src="/Images/ORK_Logo.png"
-    alt="CA Firm Logo"
-    className="footer-logo-image"
-  />
+<img src={`${import.meta.env.BASE_URL}Images/ORK_Logo.png`} alt="Logo" />
 
   <div>
     <h2>Omkar Kulkarni & Associates</h2>
